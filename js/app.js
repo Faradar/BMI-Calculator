b3b43fd5c6e264a31f65b987bfb109a3a68ab0a2
Faradar <tomi_compuamste@hotmail.com>
2023-06-19 14:35:38 -0300

    First draft finished

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
-// Crear un algoritmo con un condicional.
-// Crear un algoritmo utilizando un ciclo.
-// Utiliza funciones
+// At least one conditional
+// At least one cycle
+// At least one function
+
+// Create main variables
+let height;
+let weight;
+
+// Cycle that checks that the entered values in the prompts are correct
+do {
+    height = (parseInt(prompt("Enter your height in cm"))) / 100;
+} while (validate(height));
+
+do {
+    weight = parseInt(prompt("Enter your weight in kg"));
+} while (validate(weight));
+
+// Create the bmi constant
+const bmi = (weight / Math.pow(height, 2)).toFixed(1);
+
+// Conditional that checks the resulting bmi and provides the type of weight associated with it
+if (bmi < 18.5) {
+    alert(`Your BMI is: ${bmi}\nYour BMI suggests you're an underweight`);
+} else if (bmi >= 18.5 && bmi < 25) {
+    alert(`Your BMI is: ${bmi}\nYour BMI suggests you're a healthy weight`);
+} else if (bmi >= 25 && bmi < 30) {
+    alert(`Your BMI is: ${bmi}\nYour BMI suggests you're an overweight`);
+} else {
+    alert(`Your BMI is: ${bmi}\nYour BMI suggests you're an obese`);
+}
+
+// function that validates whether the parameter is a positive number.
+function validate(number) {
+    if (isNaN(number)) {
+        alert("You did not enter a number, please try again");
+        return true;
+    } else if (number <= 0) {
+        alert("Positive numbers only please");
+        return true;
+    } else {
+        return false;
+    }
+}
