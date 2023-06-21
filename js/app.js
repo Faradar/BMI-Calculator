6f644905a976a6f5503b71ac44e7418b14eaecc2
Faradar <tomi_compuamste@hotmail.com>
2023-06-21 00:26:56 -0300

    Added the healthy BMI variables to the JS

--- js/app.js
+++ js/app.js
@@ -18,25 +18,27 @@ let weight;
 
 // Cycle that checks that the entered values in the prompts are correct
 do {
-    height = (parseInt(prompt("Enter your height in cm"))) / 100;
+    height = (parseInt(prompt("Enter your height in cm"))) / 100; // Divided by 100 to go from cm to meters
 } while (validate(height));
 
 do {
     weight = parseInt(prompt("Enter your weight in kg"));
 } while (validate(weight));
 
-// Create the bmi constant
+// Create the bmi constants
 const bmi = (weight / Math.pow(height, 2)).toFixed(1);
+const healthyBmiFloor = (18.5 * Math.pow(height, 2)).toFixed(1);
+const healthyBmiCeil = (24.9 * Math.pow(height, 2)).toFixed(1);
 
 // Conditional that checks the resulting bmi and provides the type of weight associated with it
 if (bmi < 18.5) {
-    alert(`Your BMI is: ${bmi}\nYour BMI suggests you're an underweight`);
+    alert(`Your BMI is: ${bmi}\nYour BMI suggests you're an underweight. Your ideal weight for your height is between ${healthyBmiFloor}kg - ${healthyBmiCeil}kg`);
 } else if (bmi >= 18.5 && bmi < 25) {
-    alert(`Your BMI is: ${bmi}\nYour BMI suggests you're a healthy weight`);
+    alert(`Your BMI is: ${bmi}\nYour BMI suggests you're a healthy weight. Your ideal weight for your height is between ${healthyBmiFloor}kg - ${healthyBmiCeil}kg`);
 } else if (bmi >= 25 && bmi < 30) {
-    alert(`Your BMI is: ${bmi}\nYour BMI suggests you're an overweight`);
+    alert(`Your BMI is: ${bmi}\nYour BMI suggests you're an overweight. Your ideal weight for your height is between ${healthyBmiFloor}kg - ${healthyBmiCeil}kg`);
 } else {
-    alert(`Your BMI is: ${bmi}\nYour BMI suggests you're an obese`);
+    alert(`Your BMI is: ${bmi}\nYour BMI suggests you're an obese. Your ideal weight for your height is between ${healthyBmiFloor}kg - ${healthyBmiCeil}kg`);
 }
 
 // function that validates whether the parameter is a positive number.
