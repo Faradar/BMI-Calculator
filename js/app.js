d391e56e0043ff8a8c49f6094a8b525a22dac409
Faradar <tomi_compuamste@hotmail.com>
2023-07-13 16:23:58 -0300

    Added some comments to the JS

--- js/app.js
+++ js/app.js
@@ -47,6 +47,7 @@ document.getElementById('form').addEventListener('submit', (e) => {
     // Add that object to an array
     users.push(user);
 
+    // Once all is done go to the next function
     showResults();
 });
 
@@ -57,8 +58,8 @@ function showResults() {
         let [bmi, healthyBmiFloor, healthyBmiCeil] = bmiResults; //Destructured the array
         document.getElementById('bmiNumber').innerText = bmi;
 
-        // Conditional that checks the resulting bmi and provides the type of weight associated with it
-        if (isNaN(bmi) || bmi <= 0) {
+        // Conditional that checks the resulting bmi and provides the type of weight associated with it in the DOM
+        if (isNaN(bmi) || bmi <= 0) { // If the bmi results in a silly value
             document.getElementById('bmiNumber').innerText = "Wrong";
             document.getElementById('bmiText').innerHTML = `${user.name}, the numbers you provided seem to be wrong. Please make sure you are entering the correct positive numbers for your height and weight.`;
         } else if (bmi < 18.5) {
@@ -71,6 +72,7 @@ function showResults() {
             document.getElementById('bmiText').innerHTML = `${user.name}, your BMI suggests you're <span class="input-section__weight-name">obese</span>. The ideal weight for your height is between <span class="input-section__weight-number">${healthyBmiFloor}kg - ${healthyBmiCeil}kg</span>`;
         }
 
+        // Save the last inputed values in the local storage
         localStorage.setItem('user', JSON.stringify(user));
     }
 }
@@ -84,11 +86,12 @@ function calculateBMI(user) {
     return bmiResults;
 }
 
-// Function to enter the last used valued automatically
+// Function to enter the last used values automatically
 function remember() {
+    // Get the last inputed values in the local storage
     let user = JSON.parse(localStorage.getItem('user'));
 
-    // Remember the data
+    // Check if said values exists, if it does then add them otherwise don't add anything
     document.getElementById('name').value = user?.name || '';
     document.getElementById('height').value = user?.height * 100 || ''; // Multiplied by 100 to go from meters to cm
     document.getElementById('weight').value = user?.weight || '';
