81451bb3574b3dd4cc2ef2bc5fe45904cccab5a3
Faradar <tomi_compuamste@hotmail.com>
2023-07-29 00:13:00 -0300

    Made some final adjustments to the js code

--- js/app.js
+++ js/app.js
@@ -1,51 +1,15 @@
-// First Draft:
-// At least one conditional - Done
-// At least one cycle - Done
-// At least one function - Done
-// ---
-// Second Draft:
-// HTML and CSS Structure needs to start approaching the final page - Done
-// JS Variables - Done
-// Essential functions - Done
-// JS Objects - Done
-// Arrays - Done
-// Search and filter methods on the Array - Done
-// ---
-// Third Draft:
-// JSON and Local Storage - Done
-// DOM and events - Done
-// ---
-// Final Draft:
-// Incorporate libraries - Done
-// Funcion async-await, con fetch a un archivo JSON y que haga uso del try ... catch. Posiblemente agregar un finally.
-// una idea es usar la libreria de sweet alert para cuando la respuesta este mal, tipo si el fetch no llega
-// implementar email js para enviar mails y demas? mas en la clase 16. es un api que permite que te manden un mail
-// Ver si hay errores 404, si los hay hacerlos lindos (tipo con un sweet alert)
-// El flujo del local storage tiene que ser setitem, getitem y el remove. Son 3 pasos.
-// Sin codigo comentado
-// Sin console log
-// Revisar que todo lo de la primera, second y third draft sigue estando completo
-// Hacer una revision final del powerpoint de la clase 17 para revisar que cumpli con todo lo pedido de ahi tambien
-// limit the amount of digits you can put in the height and weight inputs, add a possible comma to this limitation. Maybe add a sweet alert popup when you try to write while on the limit
-// add good commentary to the html, sass and js code
-// ---
-
-
-// 1. Tengo que guardar los datos calculados al local storage
-// 2. tengo que hacer que esos datos aparezcan usando el local storage
-// 3. tengp que hacer que esos datos sean recordados y sigan existiendo en otra sesion
-// 4. tengo que hacer que esos datos sean eliminados cuando aprete el boton de eliminar
-
-
 // Execute this function upon startup
 remember();
 
 // Add an event listener to the height and weight inputs
 const heightInput = document.getElementById('height');
-heightInput.addEventListener('input', limitDigits);
-
 const weightInput = document.getElementById('weight');
+
+heightInput.addEventListener('input', limitDigits);
 weightInput.addEventListener('input', limitDigits);
+
+// Create a variable to keep track of the last time an alert was shown
+let lastAlertTime = 0;
 
 // Get the form element and trigger a function upon its submission
 const formSubmit = document.getElementById('form');
@@ -61,11 +25,11 @@ formSubmit.addEventListener('submit', (e) => {
     let date = luxon.DateTime.now();
     date = {
         day: date.day,
+        hour: date.hour,
+        minute: date.minute,
         monthLong: date.monthLong,
         year: date.year,
-        zoneName: date.zoneName,
-        hour: date.hour,
-        minute: date.minute
+        zoneName: date.zoneName
     };
 
     // Generate a unique userId for the current user
@@ -76,6 +40,9 @@ formSubmit.addEventListener('submit', (e) => {
 
     // Add that object to an array
     users.push(user);
+
+    // Save the users array to local storage
+    localStorage.setItem('users', JSON.stringify(users));
 
     // Once all is done go to the next function
     showResults(user);
--- js/functions.js
+++ js/functions.js
@@ -31,14 +31,141 @@ async function remember() {
     }
 }
 
+// Retrieve data from JSON file
+async function fetchData() {
+    try {
+        const response = await fetch('./JSON/people.json');
+
+        // Check if the server response is successful
+        if (!response.ok) {
+            throw new Error('Error getting JSON file');
+        }
+
+        const data = await response.json();
+
+        // Process the data and return the result
+        return data.people;
+    } catch (error) {
+        // Error handling
+        console.error('Error getting the data:', error.message);
+        return []; // Return an empty array if there was an error
+    }
+}
+
+// Function to create cards for the user data
+function insertData(user) {
+    const userDiv = document.createElement('div');
+    const date = user.date;
+    const formattedTimer = formatTimer(date.hour, date.minute);
+    let ordinalIndicator = setOrdinal(date.day);
+
+    userDiv.innerHTML = `
+        <div class="result-section__div card">
+            <p>${user.name} calculated their BMI the ${date.day}${ordinalIndicator} of ${date.monthLong} ${date.year} on ${date.zoneName} at ${formattedTimer} and had a result of <span class="result-section__bmi">${user.bmi.bmi}</span></p>
+            <button type="button" class="result-section__button"><i class="fa-solid fa-square-xmark" style="color: #f59942;"></i></button>
+        </div>
+    `;
+
+    document.getElementById('resultData').appendChild(userDiv);
+
+    removeData(user, userDiv);
+}
+
+// Function to add a 0 in front of single digit hours and minutes for better clarity
+function formatTimer(hours, minutes) {
+    // Convert hours and minutes to strings
+    const formattedHours = hours.toString().padStart(2, '0');
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+
+    // Combine the formatted hours and minutes with a colon between them
+    const formattedTimer = `${formattedHours}:${formattedMinutes}hs`;
+
+    return formattedTimer;
+}
+
+// Function to set the ordinal indicator
+function setOrdinal(day) {
+    let indicator = '';
+
+    // Change the ordinal indicator depending on the day
+    if (day === 1) {
+        indicator = 'st';
+    } else if (day === 2) {
+        indicator = 'nd';
+    } else if (day === 3) {
+        indicator = 'rd';
+    } else {
+        indicator = 'th';
+    }
+
+    return indicator;
+}
+
+function removeData(user, userDiv) {
+    // Manipulate the delete button
+    const deleteButton = userDiv.querySelector('.result-section__button');
+    // Set the data-id attribute to the user's unique identifier
+    deleteButton.setAttribute('data-id', user.id);
+    // Add event listener
+    deleteButton.addEventListener('click', () => {
+        const userId = deleteButton.getAttribute('data-id');
+
+        // Find the index of the user with the matching userId in the users array
+        const userIndex = users.findIndex(user => user.id === userId);
+
+        if (userIndex !== -1) {
+            // Remove the user from the users array
+            users.splice(userIndex, 1);
+
+            // Check if the users array is empty and remove the local storage entry if it is
+            if (users.length === 0) {
+                localStorage.removeItem('users');
+            } else {
+                // Update the local storage with the updated users array
+                localStorage.setItem('users', JSON.stringify(users));
+            }
+        }
+
+        // Remove the parent div when the button is clicked
+        userDiv.remove();
+        Toastify({
+            text: "Deleted",
+            duration: 700,
+            style: {
+                background: "#f59942"
+            }
+        }).showToast();
+    });
+}
+
 // Limit the height and weight inputs to prevent silly numbers
 function limitDigits(event) {
     const input = event.target;
     const value = input.value;
-    const maxLength = value.includes('.') ? 4 : 3;
+    const maxLength = 3;
 
     // Remove any non-digit characters from the input
-    const digitsOnly = value.replace(/[^\d.,]/g, '');
+    const digitsOnly = value.replace(/[^\d]/g, '');
+
+    if (digitsOnly.length > 3) {
+        const currentTime = Date.now();
+        // Calculate the time since the last alert was shown
+        const timeSinceLastAlert = currentTime - lastAlertTime;
+
+        // Check if enough time has passed since the last alert
+        if (timeSinceLastAlert > 1000) { // Set the desired delay in milliseconds (1000ms = 1 second)
+            Toastify({
+                text: "Please enter only 3 numbers",
+                duration: 700,
+                style: {
+                    background: "red"
+                }
+            }).showToast();
+
+            // Update the lastAlertTime variable to the current time
+            lastAlertTime = currentTime;
+        }
+    }
 
     // Limit the number of digits to maxLength
     const limitedValue = digitsOnly.slice(0, maxLength);
@@ -57,15 +184,18 @@ function calculateBMI(height, weight) {
     return bmiResults;
 }
 
+// Function to generate a randomize unique ID
 function generateUserId() {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const length = 8; // Adjust the length of the generated ID as needed
     let userId = '';
 
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        userId += characters.charAt(randomIndex);
-    }
+    do {
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            userId += characters.charAt(randomIndex);
+        }
+    } while (users.some(user => user.id === userId)); // Check if the generated ID already exists in the users array
 
     return userId;
 }
@@ -90,103 +220,5 @@ function showResults(user) {
         document.getElementById('bmiText').innerHTML = `${user.name}, your BMI suggests you're <span class="input-section__weight-name">obese</span>. The ideal weight for your height is between <span class="input-section__weight-number">${hf}kg - ${hc}kg</span>`;
     }
 
-    // Save the users array to local storage
-    localStorage.setItem('users', JSON.stringify(users));
-
     insertData(user);
-}
-
-function insertData(user) {
-    const userDiv = document.createElement('div');
-    const date = user.date;
-    const formattedTimer = formatTimer(date.hour, date.minute);
-    let ordinalIndicator;
-
-    // Change the ordinal indicator depending on the day
-    if (date.day === 1) {
-        ordinalIndicator = 'st';
-    } else if (date.day === 2) {
-        ordinalIndicator = 'nd';
-    } else if (date.day === 3) {
-        ordinalIndicator = 'rd';
-    } else {
-        ordinalIndicator = 'th';
-    }
-
-    userDiv.innerHTML = `
-        <div class="result-section__div card">
-            <p>${user.name} calculated their BMI the ${date.day}${ordinalIndicator} of ${date.monthLong} ${date.year} on ${date.zoneName} at ${formattedTimer} and had a result of <span class="result-section__bmi">${user.bmi.bmi}</span></p>
-            <button type="button" class="result-section__button"><i class="fa-solid fa-square-xmark" style="color: #f59942;"></i></button>
-        </div>
-    `;
-
-    document.getElementById('resultData').appendChild(userDiv);
-
-    const deleteButton = userDiv.querySelector('.result-section__button');
-    // Set the data-id attribute to the user's unique identifier
-    deleteButton.setAttribute('data-id', user.id);
-    // Add event listener to the delete button
-    deleteButton.addEventListener('click', () => {
-        const userId = deleteButton.getAttribute('data-id');
-
-        // Find the index of the user with the matching userId in the users array
-        const userIndex = users.findIndex(user => user.id === userId);
-
-        if (userIndex !== -1) {
-            // Remove the user from the users array
-            users.splice(userIndex, 1);
-
-            // Update the local storage with the updated users array
-            localStorage.setItem('users', JSON.stringify(users));
-        }
-
-        // Remove the parent div when the button is clicked
-        userDiv.remove();
-        Toastify({
-            text: "Deleted",
-            duration: 700,
-            style: {
-                background: "#f59942"
-            }
-        }).showToast();
-    });
-}
-
-function formatTimer(hours, minutes) {
-    // Convert hours and minutes to strings
-    const formattedHours = hours.toString().padStart(2, '0');
-    const formattedMinutes = minutes.toString().padStart(2, '0');
-
-    // Combine the formatted hours and minutes with a colon between them
-    const formattedTimer = `${formattedHours}:${formattedMinutes}hs`;
-
-    return formattedTimer;
-}
-
-// Retrieve data from JSON file
-async function fetchData() {
-    try {
-        const response = await fetch('./JSON/people.json');
-
-        // Check if the server response is successful
-        if (!response.ok) {
-            throw new Error('Error getting JSON file');
-        }
-
-        const data = await response.json();
-
-        // data.people.forEach((person) => {
-        //     insertData(person);
-        // });
-
-        // Process the data and return the result
-        return data.people;
-    } catch (error) {
-        // Error handling
-        console.error('Error getting the data:', error.message);
-        return []; // Return an empty array if there was an error
-    } finally {
-        // Code that will always be executed, whether there is an error or not
-        console.log('The fetchData function has finished');
-    }
 }
